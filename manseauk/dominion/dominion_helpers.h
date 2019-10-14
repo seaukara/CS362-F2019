a29b8e700c0b62998ed1c51ef6eb0bf0bd023e2c
seaukara <karamanseau@gmail.com>
2019-10-13 19:43:31 -0700

    completed assignment 2

--- manseauk/dominion/dominion_helpers.h
+++ manseauk/dominion/dominion_helpers.h
@@ -9,6 +9,12 @@ int discardCard(int handPos, int currentPlayer, struct gameState *state,
                 int trashFlag);
 int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
+
+int mineEffect(int choice1, int choice2, struct gameState *state, int handPos);
+int baronEffect(int choice2, struct gameState *state);
+int minionEffect(int choice1, int choice2, struct gameState *state, int handPos);
+int tributeEffect(struct gameState *state);
+int ambassadorEffect(int choice1, int choice2, struct gameState *state, int handPos);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
 
